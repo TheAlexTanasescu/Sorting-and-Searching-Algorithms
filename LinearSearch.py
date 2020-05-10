3c9599dca9b748d4019651381f85a3ba0396096f
TheAlexTanasescu <alskiboy@yahoo.com>
2020-05-09 18:31:08 -0600

    Improved linear search

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,13 +1,25 @@
-def linearSearch(array, size, valueToFind):
+def linearSearch(array, target):
+    size = len(array)
     for i in range(0, size):
-        if (valueToFind == array[i]):
+        if (target == array[i]):
             return i
     return -1
 
-array = [5,3,9,21,13,67,8,11,42]
-valueToFind = 67
-size = len(array)
-result = linearSearch(array, size, valueToFind)
+#Driver code
+array = []
+counter = 0
+
+numItems = int(input("Enter how many items you want to have in the array: "))
+
+
+while(counter < numItems):
+    number = int(input("Enter a number to add to the array: "))
+    array.append(number)
+    counter += 1
+    print("Array is: ",array)
+target = int(input("Enter the value you're trying to find: "))
+#print(array)    
+result = linearSearch(array, target)
 if (result == -1):
     print("number not found")
 else:
